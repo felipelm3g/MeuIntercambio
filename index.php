eab5d3c097bd355c6ca9ffa9d89ba7f008f7519c
Felipe Lopes <felipelm07@gmail.com>
2019-02-21 12:08:55 -0300

    Felipe
    21.02.2019

--- index.php
+++ index.php
@@ -5,13 +5,63 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <title>Hello, world!</title>
-        
+        <title>Meu Intercâmbio</title>
+
         <!-- Favicon -->
         <link rel="shortcut icon" href="imgs/favicon.ico" />
     </head>
     <body>
-        <h1>Hello, world!</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="imgs/icone.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                Meu Intercâmbio
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Início</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Link </a>
+                    </li>
+                    <li class="nav-item dropdown active">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Destinos
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/1280px-Flag_of_Brazil.svg.png" width="28.5" height="20" style="border-radius: 3px;box-shadow: 1px 1px 2px 0px rgba(125,125,125,1);" />&nbsp; Austrália</a>
+                            <a class="dropdown-item" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1200px-Flag_of_Canada_%28Pantone%29.svg.png" width="28.5" height="20" style="border-radius: 3px;box-shadow: 1px 1px 2px 0px rgba(125,125,125,1);" />&nbsp; Canadá</a>
+                            <a class="dropdown-item" href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/2000px-Flag_of_the_United_States.svg.png" width="28.5" height="20" style="border-radius: 3px;box-shadow: 1px 1px 2px 0px rgba(125,125,125,1);" />&nbsp; Estados Unidos</a>
+                            <a class="dropdown-item" href="#"><img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_700,h_420/https://www.canallondres.tv/wp-content/uploads/2017/10/Bandeira-da-Gra-Bretanha.png" width="28.5" height="20" style="border-radius: 3px;box-shadow: 1px 1px 2px 0px rgba(125,125,125,1);" />&nbsp; Inglaterra</a>
+                            <a class="dropdown-item" href="#"><img src="https://i1.wp.com/www.eurodicas.com.br/wp-content/uploads/2018/05/cores-da-bandeira-da-irlanda.jpg?zoom=2.625&w=1000&ssl=1" width="28.5" height="20" style="border-radius: 3px;box-shadow: 1px 1px 2px 0px rgba(125,125,125,1);" />&nbsp; Irlanda</a>
+                            <a class="dropdown-item" href="#"><img src="https://www.estudopratico.com.br/wp-content/uploads/2017/02/significado-da-bandeira-de-malta.jpg" width="28.5" height="20" style="border-radius: 3px;box-shadow: 1px 1px 2px 0px rgba(125,125,125,1);" />&nbsp; Malta</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Promocionais</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+                <!--                <form class="form-inline my-2 my-lg-0">
+                                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                </form>-->
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-outline-info my-2 my-sm-0">
+                        <input type="radio" name="options" id="option1" autocomplete="off" checked> Criar
+                    </label>
+                    <label class="btn btn-outline-info my-2 my-sm-0">
+                        <input type="radio" name="options" id="option2" autocomplete="off"> Login
+                    </label>
+                </div>
+
+            </div>
+        </nav>
 
         <?php
         // put your code here
