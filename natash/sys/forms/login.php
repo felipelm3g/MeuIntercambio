ed3d155ebd998de071fc97c07748cb10a8bbe17c
Felipe Lopes <felipelm07@gmail.com>
2019-03-26 13:30:33 -0300

    Felipe
    ajuste

--- natash/sys/forms/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../class/User.php';
-$user = new User($_POST["email"],$_POST["senha"]);
-$return = $user->logar();
-echo json_encode($return);
-?>
-
--- natash/teste.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'sys/class/User.php';
-
-
-
